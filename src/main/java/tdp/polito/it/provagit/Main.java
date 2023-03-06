0b3e2814acde9a7c087dcc2cbca8471f485762f4
luciaruffati <luciaruffati@localhost>
2023-03-06 09:27:09 +0100

    aggiunto un commento nel main

--- src/main/java/tdp/polito/it/provagit/Main.java
+++ src/main/java/tdp/polito/it/provagit/Main.java
@@ -3,5 +3,7 @@ package tdp.polito.it.provagit;
 public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
+        
+        // prova per vedere come funziona commit
     }
 }
